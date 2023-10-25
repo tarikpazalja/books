88f1dbec4af1f441e31d9e2229f21a69f0042c9f
Tarik Pazalja <tarikpazalja@gmail.com>
2023-10-25 09:05:25 +0200

    prvi push

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { BooksDatabase } from './db';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().decorate('db', new BooksDatabase)
+
+app.get('/books', ({ db }) => db.getBooks());
+app.post('/books', ({db, body }) => db.addBook(body), {
+  body: t.Object({
+    name: t.String(),
+    author: t.String()
+  })
+})
+app.put('/books', ({ db, body }) => db.updateBook(body.id, {name: body.name, author: body.author }), {
+  body: t.Object({
+    id: t.Number(),
+    name: t.String(),
+    author: t.String()
+  })
+})
+
+app.get('/books/:id', ({db, params }) => db.getBook(parseInt(params.id)))
+app.delete('/books/:id', ({db, params }) => db.deleteBook(parseInt(params.id)))
+
+app.listen(8081)
+
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
