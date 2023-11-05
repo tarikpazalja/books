f382e0bf2ec17e3154438908d8437c6bbd397fa7
Tarik Pazalja <tarikpazalja@gmail.com>
2023-11-05 17:06:29 +0100

    ddd

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
-import { Elysia, t } from "elysia";
+import { Elysia, t } from 'elysia';
 import { BooksDatabase } from './db';
 
-const app = new Elysia().decorate('db', new BooksDatabase)
+const app = new Elysia().decorate('db', new BooksDatabase());
 
 app.get('/books', ({ db }) => db.getBooks());
-app.post('/books', ({db, body }) => db.addBook(body), {
+app.post('/books', ({ db, body }) => db.addBook(body), {
   body: t.Object({
     name: t.String(),
-    author: t.String()
-  })
-})
-app.put('/books', ({ db, body }) => db.updateBook(body.id, {name: body.name, author: body.author }), {
-  body: t.Object({
-    id: t.Number(),
-    name: t.String(),
-    author: t.String()
-  })
-})
+    author: t.String(),
+  }),
+});
+app.put(
+  '/books',
+  ({ db, body }) =>
+    db.updateBook(body.id, { name: body.name, author: body.author }),
+  {
+    body: t.Object({
+      id: t.Number(),
+      name: t.String(),
+      author: t.String(),
+    }),
+  }
+);
 
-app.get('/books/:id', ({db, params }) => db.getBook(parseInt(params.id)))
-app.delete('/books/:id', ({db, params }) => db.deleteBook(parseInt(params.id)))
+app.get('/books/:id', ({ db, params }) => db.getBook(parseInt(params.id)));
+app.delete('/books/:id', ({ db, params }) =>
+  db.deleteBook(parseInt(params.id))
+);
 
-app.listen(8081)
-
+app.listen(8081);
 
 console.log(
-  `🦊 Elysia is rrunning at http://${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is rhhhhhhrunning at http://${app.server?.hostname}:${app.server?.port}`
 );
